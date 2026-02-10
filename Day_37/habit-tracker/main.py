0678480e2022b528b4f31e11115255843dedb935
Dominik Wędzina <dominik.wedzina@onet.pl>
2026-02-10 11:45:42 +0100

    finish day 37

--- Day_37/habit-tracker/main.py
+++ Day_37/habit-tracker/main.py
@@ -1,11 +1,16 @@
 import requests
+from datetime import datetime as dt
 
-# $ curl -X POST https://pixe.la/v1/users -d '{"token":"thisissecret", "username":"a-know", "agreeTermsOfService":"yes", "notMinor":"yes"}'
-# {"message":"Success.","isSuccess":true}
+from dateutil.relativedelta import relativedelta
+
 api = "https://pixe.la/v1"
 
 USERNAME= "dominikwedzina"
-TOKEN = ""
+TOKEN = "gshe78rht873ehuh3twert"
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
 
 # user_params = {
 #     "token": TOKEN,
@@ -21,10 +26,37 @@ TOKEN = ""
 graph_params = {
     "id": "graph1",
     "name": "Books Pages Graph",
-    "unit": "commit",
+    "unit": "pages",
     "type": "int",
     "color": "sora"
 }
+# res = requests.post(f"{api}/users/{USERNAME}/graphs", json=graph_params, headers=headers)
+# res.raise_for_status()
+# print(res.text)
+first_day = dt.today() - relativedelta(months=1, days=21)
 
-res = requests.post(f"{api}/users/{USERNAME}/graphs", json=graph_params)
+
+# pixel_list = []
+today = dt.today().strftime('%Y%m%d')
+# while first_day.strftime('%Y%m%d') != today:
+#     pixel_list.append({"date": first_day.strftime('%Y%m%d'), "quantity": "10"})
+#     first_day = first_day + relativedelta(days=1)
+
+
+# post_pixel_params = {
+#     "date": today,
+#     "quantity": "10",
+# }
+
+post_pixel_params = {
+    "date": first_day.strftime('%Y%m%d'),
+    "quantity": "10",
+}
+
+# res = requests.post(f"{api}/users/{USERNAME}/graphs/graph1", json=post_pixel_params, headers=headers)
+# res.raise_for_status()
+# print(res.text)
+
+res = requests.delete(f"{api}/users/{USERNAME}/graphs/graph1/{first_day.strftime('%Y%m%d')}", headers=headers)
 res.raise_for_status()
+print(res.text)
