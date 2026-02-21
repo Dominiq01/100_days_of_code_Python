623c1c5876a43e92e09dc4de631beaa703a2a782
Dominik Wędzina <dominik.wedzina@onet.pl>
2026-02-21 20:41:11 +0100

    finish day 46

--- Day_46/main.py
+++ Day_46/main.py
@@ -2,13 +2,13 @@
 import requests
 from bs4 import BeautifulSoup
 from ytmusicapi import YTMusic
-
+import os
 header = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"
+    "User-Agent": os.environ.get("HEADER")
 }
 
 URL = "https://www.officialcharts.com/charts/singles-chart/"
-user_input = input("Which year do you want to travel to? Type date in this format YYY-MM-DD: ")
+user_input = input("Which year do you want to travel to? Type date in this format YYYY-MM-DD: ")
 
 res = requests.get(f"{URL}/{user_input}", headers=header)
 
@@ -20,6 +20,24 @@ soup = BeautifulSoup(contents, "html.parser")
 list_of_titles = [title.text for title in soup.select(".description.block p a.chart-name span:last-child")]
 
 print(list_of_titles)
-# 867051867517-pdl6ntjd9cmf7hjo2aum90o1s28i9gb0.apps.googleusercontent.com
-YTMusic.setup_oauth()
-ytmusic = YTMusic()
+
+ytmusic = YTMusic('browser.json')
+
+results = []
+for title in list_of_titles:
+    try:
+        result = ytmusic.search(title, filter="songs")
+        videoID = result[0]['videoId']
+        results.append(videoID)
+        print(result)
+    except IndexError:
+        print("Song doesn't exists on yt music :(")
+
+playlist_title = f"Top 100 Songs from {user_input}"
+my_new_playlist = ytmusic.create_playlist(
+    title=playlist_title,
+    description="My playlist with top 100 songs from the day I was born.",
+    video_ids = results
+)
+
+print(f"Success! Playlist created. ID: {my_new_playlist}")
