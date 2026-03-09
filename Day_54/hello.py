656aa6f137eda64d7d328a9bb3cf864a46956c1a
Dominik Wędzina <dominik.wedzina@onet.pl>
2026-03-09 11:24:22 +0100

    finish day 54

--- Day_54/hello.py
+++ Day_54/hello.py
@@ -1,7 +1,30 @@
 from flask import Flask
+import time
+# app = Flask(__name__)
+#
+# @app.route("/")
+# def hello():
+#     return "Hello, World!"
+#
+# if __name__ == "__main__":
+#     app.run()
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+def delay_decorator(func):
+    def wrapper_function():
+        time.sleep(2)
+        func()
+    return wrapper_function
+
+@delay_decorator
+def say_hello():
+    print("Hello, World!")
+
+def say_bye():
+    print("Bye, World!")
+
+decorated_function = delay_decorator(say_bye)
+
+
+say_hello()
+decorated_function()
