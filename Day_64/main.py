be51454ff6b9f5ca186f1751d5edd4090c117f59
Dominik Wędzina <dominik.wedzina@onet.pl>
2026-03-16 11:48:19 +0100

    finish day 64

--- Day_64/main.py
+++ Day_64/main.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template, redirect, url_for, request
+import os
+
+from flask import Flask, render_template, redirect, url_for, request, session
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Float
+from sqlalchemy import Integer, String, Float, desc
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 from rate_movie_form import RateMovieForm
 from add_movie_form import AddMovieForm
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -22,8 +26,12 @@ class Base(DeclarativeBase):
 
 
 db = SQLAlchemy(model_class=Base)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-movies-collection.db"
 db.init_app(app)
+headers = {
+    "accept": "application/json",
+    "Authorization": os.getenv('TOKEN')
+}
 
 
 # CREATE TABLE
@@ -42,23 +50,47 @@ class Movie(db.Model):
 @app.route("/")
 def home():
     with app.app_context():
-        results = db.session.execute(db.select(Movie).order_by(Movie.ranking))
+        results = db.session.execute(db.select(Movie).order_by(desc(Movie.rating)))
         all_movies = results.scalars().all()
-        books_count = len(all_movies)
-        print(all_movies)
+        for i in range(0, len(all_movies)):
+            all_movies[i].ranking = i + 1
     return render_template("index.html", all_movies=all_movies)
 
-@app.route("/add/<movie>", methods=['GET', 'POST'])
-def add(movie=None):
+
+@app.route("/add_movie/<movie_id>")
+def add_movie(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}"
+
+    params = {
+        "language": "en-US",
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    movie = response.json()
+    if movie:
+        with app.app_context():
+            new_movie = Movie(
+                id=int(movie['id']),
+                title=movie['original_title'],
+                year=movie['release_date'],
+                description=movie['overview'],
+                rating=0,
+                ranking=0,
+                review="",
+                img_url=f"https://image.tmdb.org/t/p/w500{movie['poster_path']}"
+            )
+            db.session.add(new_movie)
+            db.session.commit()
+    return redirect(f"/edit/{movie_id}")
+
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
     form = AddMovieForm()
     if request.method == 'POST':
         name = form.name.data
         url = "https://api.themoviedb.org/3/search/movie"
-
-        headers = {
-            "accept": "application/json",
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MDNlNjU3ODA5NzcwZjc0YmZjZThmOTE0NjZmNjg2YyIsIm5iZiI6MTc3MzU5ODE1MS41OTMsInN1YiI6IjY5YjZmNWM3NWQ3N2I0OWY0ZTVjOWU5MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GDoJjABAnYFltijimj88IgXnNloRlM1YOTKbbBAbQnc"
-        }
 
         params = {
             "query": name,
@@ -70,26 +102,10 @@ def add(movie=None):
         response = requests.get(url, headers=headers, params=params)
         response.raise_for_status()
         data = response.json()['results']
-        print(data)
         return render_template("select.html", data=data)
 
-
-    with app.app_context():
-        new_movie = Movie(
-            title=movie['original_title'],
-            year=movie['release_date'],
-            description=movie['overview'],
-            rating=0,
-            ranking=0,
-            review="",
-            img_url=f"https://image.tmdb.org/t/p/w500{movie['poster_path']}"
-        )
-        print(new_movie)
-        db.session.add(new_movie)
-        db.session.commit()
-    return redirect('/')
-
     return render_template("add.html", form=form)
+
 
 @app.route("/edit/<movie_id>", methods=['GET', 'POST'])
 def edit(movie_id):
@@ -111,7 +127,6 @@ def delete(movie_id):
         db.session.delete(movie_to_delete)
         db.session.commit()
         return redirect("/")
-
 
 
 if __name__ == '__main__':
